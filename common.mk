58a1193aa6d71c65d3e0ef2f53efa3daa8df5c08
Nick Reuter <nreuter85@gmail.com>
2022-07-31 20:57:14 -0500

    sm4350-common: build qti light hal
    Change-Id: I4e9e4dd551f16757cd9375868231f1a6eab20c36

--- common.mk
+++ common.mk
@@ -304,6 +304,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.lights-service.qti \
+    lights.qcom
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.oneplus_holi
